f995bba51a3036bc56dfcb2c234b0c9c160cef20
Alejandro <eloso85@gmail.com>
2020-09-25 21:56:38 -0500

    adding stuff

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import 'firebase/auth';
 
 import {useAuthState} from 'react-firebase-hooks/auth';
 import {useCollectionData} from 'react-firebase-hooks/firestore';
-
+//hello
 firebase.initializeApp({
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: "chatbot-452c4.firebaseapp.com",
